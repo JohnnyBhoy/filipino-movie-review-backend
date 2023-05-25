58b2f567f71912dc5dfd960d603cd4f349565211
Johnny Antiojo Jr <jrantiojobhoy@gmail.com>
2023-05-25 10:10:23 +0800

    fix email api

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const path = require('path');
 const express = require("express");
 require("express-async-errors");
 const cors = require("cors");
 const morgan = require("morgan");
-const { errorHandler } = require("./middlewares/error");
+const {errorHandler} = require("./middlewares/error");
 const userRouter = require("./routes/user");
 const actorRouter = require("./routes/actor");
 const movieRouter = require("./routes/movie");
 const adminRouter = require("./routes/admin");
-const { handleNotFound } = require("./utils/helpers");
+const {handleNotFound} = require("./utils/helpers");
 require("dotenv").config();
 require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 app.use("/api/user", userRouter);
 app.use("/api/actor", actorRouter);
@@ -21,6 +23,9 @@ app.use("/api/movie", movieRouter);
 app.use("/api/admin", adminRouter);
 app.use("/*", handleNotFound);
 app.use(errorHandler);
-app.listen(8000, () => {
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
   console.log("The port is listening to port 8000");
 });
